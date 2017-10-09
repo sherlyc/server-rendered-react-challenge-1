7610945ca96780ca289524031c806aa8c60db50f
Joshua Vial <joshua@enspiral.com>
2017-10-09 15:28:50 +1300

    testing component was called when button clicked

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import sinon from 'sinon'
 
 import {shallow} from 'enzyme'
 import {TaskList} from './TaskList'
 
 it('renders a list of tasks', () => {
   const tasks = ['one', 'two', 'three']
-  const wrapper = shallow(<TaskList tasks={tasks} remove={sinon.stub()}/>)
+  const wrapper = shallow(<TaskList tasks={tasks} />)
   expect(wrapper.find('li').length).toBe(3)
 })
+
+it('calls remove when button clicked', () => {
+  const tasks = ['one', 'two', 'three']
+  const remove = jest.fn()
+  const wrapper = shallow(<TaskList tasks={tasks} remove={remove}/>)
+  wrapper.find('#delete-0').simulate('click')
+  expect(remove).toHaveBeenCalledWith(0)
+})
+
