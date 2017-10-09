2d467bc534bd77a8da0d5452d964121a1210bd6a
Joshua Vial <joshua@enspiral.com>
2017-10-09 17:16:37 +1300

    setting up custom start script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./src/server/server.js')
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
