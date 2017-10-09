429c32471f330fb018fd5ed8b8e78f14f37de40e
Joshua Vial <joshua@enspiral.com>
2017-10-09 22:29:56 +1300

    step 3 - passing down the redux store

--- src/server/server.js
+++ src/server/server.js
@@ -5,15 +5,18 @@ import { renderToString } from 'react-dom/server'
 
 import express from 'express'
 
+import shuffle from '../utils/shuffle'
 import reducers from '../reducers'
 import App from '../components/App'
 const app = express()
 
 app.get('/', (req, res) => {
   const store = createStore(reducers, {})
-  const html = renderToString(<Provider store={store}><App/></Provider>)
+  const shuffledStore = createStore(reducers, {tasks: shuffle(store.getState().tasks)})
+  const html = renderToString(<Provider store={shuffledStore}><App/></Provider>)
   res.send(`<body>
     <div id='root'>${html}</div>
+    <script>window.__INITIAL_STATE__ = ${JSON.stringify(shuffledStore.getState())}</script>
     <script type='text/javascript' src='/bundle.js'></script>
     </body>
   `)
--- src/store.js
+++ src/store.js
@@ -1,5 +1,12 @@
 import {createStore} from 'redux'
 
 import reducers from './reducers'
+let initialState = {}
+if (window.__INITIAL_STATE__) {
+  initialState = window.__INITIAL_STATE__
+  delete window.__INITIAL_STATE__
+}
 
-export default createStore(reducers, {})
+console.log(initialState)
+
+export default createStore(reducers, initialState)
